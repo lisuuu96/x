071a905e9276a9d0822c9be7f9ac9f24338794b2
lisuuu96 <adrian.lis1996@gmail.com>
2022-12-31 19:41:46 +0100

    try

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function equation (num1,num2)={
+function equation (num1,num2){
     let sum =num1 +num2;console.log('Wynik dodawania wynosi x');
     let substract = num1 - num2;console.log('wynik odejmowania X');
     let multiply = num1 * num2;console.log('wynik mnozenia wynosi x');
     if(sum<0 & substract<0,multiply<0) console.log('Wynik jest nieprawidlowy');
-    
+}
